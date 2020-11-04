6066a38ac3e61f45e93b55a8da09b50428d70ae3
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2020-11-03 22:01:37 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,13 @@ function vowel (text: string) {
 function consonant (text: string) {
     return text.charAt(randint(0, text.length - 1))
 }
+function Krule () {
+    return Krules[randint(0, Krules.length - 1)]
+}
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (Krule()))
+})
+let Krules: string[] = []
 let Kconst = "bcDgHjlmnpqQStvwy'''"
 let Kvowel = "aeouyI"
-let Krules = ["CVVC", "CVC", "CCVVC", "CVVC", "CV"]
+Krules = ["CVVC", "CVC", "CCVVC", "CVVC", "CV"]
